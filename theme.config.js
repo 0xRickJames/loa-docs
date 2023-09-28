196fbf37b72f5e6f8675f44b5546b17a7796c4cb
0xrickjames <0xrickjames@gmail.com>
2023-09-28 18:52:42 -0400

    added buildings

--- theme.config.js
+++ theme.config.js
@@ -15,14 +15,26 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Lands of Ascension" />
-      <meta name="og:description" content="Lands of Ascension" />
+      <meta
+        name="description"
+        content="Welcome to Lands of Ascension, the ultimate city-building adventure with a blockchain twist! Construct, manage, defend, and grow your city while attacking other Lords of Lands. Join and Ascend today!"
+      />
+      <meta
+        name="og:description"
+        content="Welcome to Lands of Ascension, the ultimate city-building adventure with a blockchain twist! Construct, manage, defend, and grow your city while attacking other Lords of Lands. Join and Ascend today!"
+      />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og2.png" />
+      <meta
+        name="twitter:image"
+        content="https://cdn.discordapp.com/attachments/939309405227339776/1157002279505891489/LoA_smaller-transformed.png?ex=651705d7&is=6515b457&hm=c35b90a44c30440d9e25b67ad13fa3bb4a7c3c84d373a6b889612f30c079d38b&"
+      />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
       <meta name="og:title" content="Lands of Ascension" />
-      <meta name="og:image" content="https://nextra.vercel.app/og2.png" />
+      <meta
+        name="og:image"
+        content="https://cdn.discordapp.com/attachments/939309405227339776/1157002279505891489/LoA_smaller-transformed.png?ex=651705d7&is=6515b457&hm=c35b90a44c30440d9e25b67ad13fa3bb4a7c3c84d373a6b889612f30c079d38b&"
+      />
       <meta name="apple-mobile-web-app-title" content="Lands of Ascension" />
       <link
         rel="apple-touch-icon"
